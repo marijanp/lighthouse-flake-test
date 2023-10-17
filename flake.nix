c796c43bf5abe7755a1213ad8fb7d5eeb8553f31
Marijan Petričević <marijan.petricevic94@gmail.com>
2023-10-17 13:35:47 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "lighthouse-flake-test";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    lighthouse-flake.url = "github:marijanp/lighthouse-flake";
+  };
+
+  outputs = inputs@{ self, flake-parts, lighthouse-flake, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      imports = [
+        lighthouse-flake.flakeModule
+      ];
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+        {
+          lighthouse = {
+            tests = {
+              "test-frontend" = {
+                dist = ./dist;
+                categories = {
+                  performance = 0.95;
+                  accessibility = 0.95;
+                  seo = 1.0;
+                  bestPractices = 0.95;
+                };
+              };
+              "test-frontend-other" = {
+                dist = ./dist;
+              };
+            };
+          };
+        };
+    };
+}
